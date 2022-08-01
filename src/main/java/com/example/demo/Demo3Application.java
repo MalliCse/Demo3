d153840b825ae861a53f601ee69e8879179fddad
mkurmala <mkurmala@MKURMALA-L-2857.evoketechnologies.com>
2022-08-01 08:33:31 -0700

    Second Commit

--- src/main/java/com/example/demo/Demo3Application.java
+++ src/main/java/com/example/demo/Demo3Application.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo3Application {
 
 	public static void main(String[] args) {
-		System.out.print("Hello")
+		System.out.print("Hello");
+		System.out.print("Malli");
 		SpringApplication.run(Demo3Application.class, args);
 	}
 
